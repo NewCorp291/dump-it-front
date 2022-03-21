60ac350ac6f3b6bf7fa26916860b429e6a94e1bb
Administrator <admin@example.com>
2022-03-21 20:14:48 +0100

    calendar events

--- src/app/calendrier/calendrier.component.ts
+++ src/app/calendrier/calendrier.component.ts
@@ -8,13 +8,18 @@ import { CalendarOptions } from '@fullcalendar/angular';
 })
 export class CalendrierComponent {
 
-  calendarOptions: CalendarOptions = {
-    initialView: 'daygridMonth'
-  };
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    calendarOptions: CalendarOptions = {
+      initialView: 'dayGridMonth',
+      events: [
+        { title: 'Carton', date: '2022-03-21' },
+        { title: 'Carton', date: '2022-03-28' },
+        { title: 'Carton', date: '2022-04-04' },
+        { title: 'Carton', date: '2022-04-09' },
+        { title: 'Carton', date: '2022-04-11' },
+        { title: 'Carton', date: '2022-04-16' },
+        { title: 'Carton', date: '2022-04-18' }
+      ],
+      eventColor: 'yellow',
+    };
+  
 }
